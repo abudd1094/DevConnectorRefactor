611bfd8e83ce9b434744a89710ba34c5eaa834a6
Alec Budd <abudd1094@gmail.com>
2019-05-31 12:58:24 +0200

    delete profile and account functionality added to frontend

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,6 +9,7 @@ const { check, validationResult } = require('express-validator/check');
 // Load Models
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current user's profile
@@ -149,9 +150,10 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // Remove posts
-
-    // Remove profile
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id })
+ 
+    // Remove user profile
     await Profile.findOneAndRemove({ user: req.user.id })
 
     // Remove user
