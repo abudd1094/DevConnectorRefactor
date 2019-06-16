1a1b10efe93fe42a672d3c0c23981a42d797b05c
Alec Budd <abudd1094@gmail.com>
2019-06-16 13:02:24 +0200

    adding profile frontend

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,6 +12,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   
   const authLinks = (
     <ul>
+      <li><Link to="/profiles">Developers</Link></li>
       <li><Link to="/dashboard">
         <i className="fas fa-user"></i>{' '} 
         {/* the below span hides the text on mobile devices */}
@@ -28,7 +29,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
   const guestLinks = (
     <ul>
-      <li><a href="#!">Developers</a></li>
+      <li><Link to="/profiles">Developers</Link></li>
       <li><Link to="/register">Register</Link></li>
       <li><Link to="/login">Login</Link></li>
     </ul>
